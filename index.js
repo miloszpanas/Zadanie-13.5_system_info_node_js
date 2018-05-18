1e279c7146d05f456aa15fcb2a4b17af8403a9ac
Miłosz Panas <miloszpanas@gmail.com>
2018-05-18 11:48:03 +0200

    Add eventEmitter

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var os = require('os');
+var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
+
+
+var emitter = new EventEmitter();
+
+emitter.on('beforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command')
+});
+emitter.on('afterCommand', function() {
+    console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if(input !== null) {
         var instruction = input.toString().trim();
+        // odpalenie zdarzenie beforeCommand (z paramterem)
+        emitter.emit('beforeCommand', instruction);
         switch (instruction) {
             case '/exit':
                 process.stdout.write('quitting app!\n');
@@ -20,9 +33,9 @@ process.stdin.on('readable', function() {
                 break;
             default:
                 process.stderr.write('wrong instruction \n');
-        }
-        
-        
+        };
+        // emitowanie zdarzenia afterCommand (bez parametru)
+        emitter.emit('afterCommand');
     }
 });
 
